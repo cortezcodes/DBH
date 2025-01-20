21a42dc6d4f3c74337f18c046c0e760f7beb8d4d
Cortez McCrary <cmccrary32@gatech.edu>
2025-01-20 08:56:33 -0500

    Feature: Hex to Decimal conversion and table

--- DBH.py
+++ DBH.py
@@ -6,7 +6,8 @@ parser.add_argument("-db", "--dec-to-bin", type=int, help="Decimal to Binary Con
 parser.add_argument("-dh", "--dec-to-hex", type=int, help="Decimal to Hexadecimal Converter")
 parser.add_argument("-bd", "--bin-to-dec", type=str, help="Binary to Decimal Converter")
 parser.add_argument("-bh", "--bin-to-hex", type=str,help="Binary to Hexadecimal Converter")
-parser.add_argument("hd", "hex-to-dec", type=str, help="Hexidecimal to Decimal Converter")
+parser.add_argument("-hd", "--hex-to-dec", type=str, help="Hexidecimal to Decimal Converter")
+parser.add_argument("-hb","--hex-to-bin", type=str, help="Hexadecimal to Binary Converter")
 parser.add_argument("-t","--table", action="store_true", help="Prints table of the number conversion" )
 
 # Constants
@@ -47,6 +48,17 @@ def binary_to_hexadecimal(bin_str: str, create_table:bool=False):
     if create_table:
         table_generator(bin_str, type=BINARY, to_type=HEXADECIMAL)
 
+def hexadecimal_to_decimal(hex_str: str, create_table:bool=False):
+    '''
+    Converts hex to decimal
+    '''
+    if hex_str[0:2] != "0x":
+        print("Hex must begin with 0x")
+        return
+    print(f"Hexadecimal {hex_str} to decimal: {int(hex_str,16)}")
+    if create_table:
+        table_generator(hex_str, type=HEXADECIMAL, to_type=DECIMAL)
+
 def table_generator(value: str, type:str, to_type:str):
     '''
     Takes a value as a string, the original type, and the type of value being converted to and prints a table to show
@@ -85,7 +97,7 @@ def table_generator(value: str, type:str, to_type:str):
                 step += 1
             table.add_row(["--","--","Total", total])
         if to_type == HEXADECIMAL:
-            table.field_names = ["step", "4-bits", "Decimal", "hex"]
+            table.field_names = ["Step", "4-bits", "Decimal", "Hex"]
             step: int = 1
             while len(value) % 4 != 0:
                 value = '0' + value
@@ -94,8 +106,20 @@ def table_generator(value: str, type:str, to_type:str):
                 dec = int(bits, 2)
                 table.add_row([step, bits, int(bits,2), hex(dec)[2:]])
                 step = step + 1
-           
-        
+    if type == HEXADECIMAL:
+        if to_type == DECIMAL:
+            table.field_names = ["Step", "Conversion", "Hex", "Decimal Value"]
+            step = 1
+            modified_value = value[2:]
+            total = 0
+            for x in range(len(modified_value)):
+                hex = modified_value[len(modified_value)-x-1]
+                hex_pos = f"16^{x}"
+                decimal_val = int(hex, 16)*(16**x)
+                total += decimal_val
+                table.add_row([step, hex_pos, hex, decimal_val])
+                step += 1        
+            table.add_row(["","", "Total", total ])
              
     print(f"{table}\n")
 
@@ -109,6 +133,8 @@ if __name__ == "__main__":
         binary_to_decimal(args.bin_to_dec, args.table)
     if args.bin_to_hex:
         binary_to_hexadecimal(args.bin_to_hex, args.table)
+    if args.hex_to_dec:
+        hexadecimal_to_decimal(args.hex_to_dec, args.table)
 
     
     
